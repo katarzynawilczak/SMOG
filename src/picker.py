45f74710f0faba54d2063daa9340efb3627e4644
hechsewa <ewa.hechsman@o2.pl>
2019-01-19 12:42:22 +0100

    pushing code with utf8 encoding

--- src/picker.py
+++ src/picker.py
@@ -30,16 +30,16 @@ class Picker(tk.Frame):
             pm.set(a)
         def timeRset(a):
             timeR.set(a)
-            
-        #Otwiera nowe okno do podawania atrybutow propagacji    
+
+        #Otwiera nowe okno do podawania atrybutow propagacji
         def propagation():
             win = tk.Toplevel(self)
-            
+
             #Propagacja
             w = tk.StringVar()
             temp = tk.IntVar()
             prec = tk.IntVar()
-            
+
             win.prompt3 = tk.Label(win, text="Start a 5h propagation with:", anchor="center")
             win.windPrompt = tk.Label(win, text="Wind[kt](format:05E):")
             win.wind = tk.Entry(win, bd =3, textvariable=w)
@@ -47,7 +47,7 @@ class Picker(tk.Frame):
             win.temperature = tk.Entry(win, bd =3, textvariable=temp)
             win.precipPrompt = tk.Label(win, text="Precipitation[mm]:")
             win.precipitation = tk.Entry(win, bd =3, textvariable=prec)
-            
+
             #Uruchamia propagacje z odpowiednimi atrybutami
             def propagateStart():
                 w = win.wind.get()
@@ -60,9 +60,9 @@ class Picker(tk.Frame):
                 self.destroy()
                 #Wywoluje funkcje z main, ktora liczy brakujace dane i uruchamia propagacje
                 main.propagationSim(w, temp, prec, 10)
-                
+
             win.startProp = tk.Button(win, text="Start propagation", command=propagateStart) #command
-        
+
             #Umieszcza przyciski i text w oknie
             win.prompt3.pack(fill="x")
             win.windPrompt.pack(side="top")
@@ -72,35 +72,35 @@ class Picker(tk.Frame):
             win.precipPrompt.pack(side="top")
             win.precipitation.pack(side="top")
             win.startProp.pack()
-            
-        
+
+
         #wybor typu pylow
         pm = tk.IntVar()
         self.prompt = tk.Label(self, text="Pick a pm type:", anchor="center")
         self.pm10 = tk.Radiobutton(self, text="PM 10", variable=pm, value=10, command=lambda:pmset(10))
         self.pm25 = tk.Radiobutton(self, text="PM2.5",variable=pm, value=25, command=lambda:pmset(25))
-        
+
         #wybor czasu trwania pomiarow
         tim = tk.IntVar()
         self.prompt2 = tk.Label(self, text="Pick a time range:", anchor="center")
         self.hour = tk.Radiobutton(self, text="24h", variable=tim, value=24, command=lambda:timeRset(24))
         self.week = tk.Radiobutton(self, text="Week", variable=tim, value=7, command=lambda:timeRset(7))
-        
+
         self.start = tk.Button(self, text="Start simulation", command=simulation)
-        
+
         self.propagate = tk.Button(self, text="Propagation", command=propagation)
-        
+
         #ustawia elementy w oknie
         self.prompt.pack(side="top", fill="x")
         self.pm10.pack(side="top")
         self.pm25.pack(side="top")
-        
+
         self.prompt2.pack(fill="x")
         self.hour.pack(side="top")
         self.week.pack(side="top")
-        
+
         self.start.pack()
-    
+
         self.propagate.pack(fill="x")
 
 if __name__ == "__main__":
